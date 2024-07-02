3d02f5d2652ba7ce79433cc50db643a59a0bb6b0
Robert Baxter <user.email>
2024-07-01 19:26:28 -0600

    add vial support for keyhive/maypad

---
+++ keyboards/keyhive/maypad/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF9, 0x22, 0x03, 0x28, 0x00, 0xB6, 0x48, 0xE6}
+#define VIAL_UNLOCK_COMBO_ROWS {0,0}
+#define VIAL_UNLOCK_COMBO_COLS {0,3}
---
+++ keyboards/keyhive/maypad/keymaps/vial/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no       # Commands for debug and configuration
