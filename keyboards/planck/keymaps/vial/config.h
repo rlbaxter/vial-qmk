2ef97ddcb2a09e7d04962df6f2487cbd00787c3b
Robert Baxter <user.email>
2024-08-27 21:33:23 -0600

    vial for planck

---
+++ keyboards/planck/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA5, 0x8E, 0xA9, 0x62, 0x8C, 0x96, 0xA8, 0x11}
+#define VIAL_UNLOCK_COMBO_ROWS {0,0}
+#define VIAL_UNLOCK_COMBO_COLS {0,3}
---
+++ keyboards/planck/keymaps/vial/rules.mk
@@ -0,0 +1,6 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no       # Commands for debug and configuration
+AUDIO_ENABLE = no
